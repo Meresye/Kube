b66ccc2ff4f8b447789ddef16197b598cdbaced7
Meresye <121928773+Meresye@users.noreply.github.com>
2025-12-23 01:31:12 -0300

    Update store.js

--- store.js
+++ store.js
@@ -3,7 +3,7 @@
     "name": "teste1",
     "version": "1.21.x",
     "download_url": "https://faithfulpack.net/",
-    "thumbnail": "https://faithfulpack.net/images/faithful_64x_banner.png",
+    "thumbnail": "https://i.ibb.co/0yzZNpRS/download-1-1.png",
     "description": "O clássico Minecraft com texturas em alta definição, mantendo a essência original do jogo.",
     "type": "Texture"
   },
@@ -11,7 +11,7 @@
     "name": "teste2",
     "version": "1.21.x",
     "download_url": "https://www.curseforge.com/minecraft/texture-packs/bare-bones-texture-pack",
-    "thumbnail": "https://media.forgecdn.net/attachments/262/342/thumbnail.png",
+    "thumbnail": "https://i.ibb.co/0yzZNpRS/download-1-1.png",
     "description": "Texturas simplificadas que dão ao jogo o visual limpo e vibrante dos trailers oficiais.",
     "type": "Texture"
   },
@@ -19,7 +19,7 @@
     "name": "teste3",
     "version": "1.21.x",
     "download_url": "https://modrinth.com/mod/sodium",
-    "thumbnail": "https://cdn.modrinth.com/data/AANobbMI/images/84687d90fd49d01f92e92ec76d3f2347209930f9.png",
+    "thumbnail": "https://i.ibb.co/0yzZNpRS/download-1-1.png",
     "description": "Mod de otimização de motor de renderização que melhora drasticamente as taxas de quadros.",
     "type": "Mods"
   },
@@ -27,7 +27,7 @@
     "name": "teste4",
     "version": "1.21.x",
     "download_url": "https://modrinth.com/mod/iris",
-    "thumbnail": "https://cdn.modrinth.com/data/yl6790u1/images/8f46a77d70428d002f23b20743b679c536442655.png",
+    "thumbnail": "https://i.ibb.co/0yzZNpRS/download-1-1.png",
     "description": "Um mod moderno de shaders para Minecraft focado em performance e compatibilidade.",
     "type": "Mods"
   }
